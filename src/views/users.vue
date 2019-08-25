fb756bf7180fe53c5920630f82c46e7d9b4b7382
托儿索 <965671101@qq.com>
2019-08-25 15:20:44 +0800

    用户列表组件制作

--- src/views/users.vue
+++ src/views/users.vue
@@ -1,11 +1,92 @@
 <template>
-    <div class="users">用户列表</div>
+  <div class="users">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!--搜索区域-->
+    <div style="margin-top: 15px;">
+      <el-input
+        placeholder="请输入内容"
+        v-model="userKey"
+        class="input-with-select"
+        style="width:300px;margin-right:10px"
+      >
+        <el-button slot="append" icon="el-icon-search"></el-button>
+      </el-input>
+      <el-button type="success">添加用户</el-button>
+    </div>
+
+    <el-table border :data="tableData" style="width: 100%;margin-top:15px">
+      <el-table-column type="index" width="50"></el-table-column>
+      <el-table-column prop="date" label="日期" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+      <el-table-column label="用户状态">
+        <template>
+          <el-switch v-model="status" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-tooltip class="item" effect="dark" content="编辑" placement="top">
+            <el-button type="primary" icon="el-icon-edit"></el-button>
+          </el-tooltip>
+          <el-tooltip class="item" effect="dark" content="分配角色" placement="top">
+            <el-button type="success" icon="el-icon-share"></el-button>
+          </el-tooltip>
+          <el-tooltip class="item" effect="dark" content="删除" placement="top">
+            <el-button type="warning" icon="el-icon-delete"></el-button>
+          </el-tooltip>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage4"
+      :page-sizes="[1, 2, 3, 4]"
+      :page-size="5"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="4"
+    ></el-pagination>
+  </div>
 </template>
 <script>
 export default {
-
+  data () {
+    return {
+      status: true,
+      userKey: '',
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }
+      ]
+    }
+  },
+  methods: {}
 }
 </script>
 <style lang="less" scoped>
-
 </style>
